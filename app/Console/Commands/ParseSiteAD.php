524cccf34d1744c9213375e73d2dd69b738b3fa1
Mikhail <k501@yandex.ru>
2016-06-27 19:16:10 +0300

    modified parser with 40* and 50* err

--- app/Console/Commands/ParseSiteAD.php
+++ app/Console/Commands/ParseSiteAD.php
@@ -27,139 +27,132 @@ class ParseSiteAD extends Command
      * @return void
      */
     private $upload_dir;
-    
+    private $target_page;
+    private $site;
+            
     public function __construct()
     {
         parent::__construct();
         $this->upload_dir = base_path() . '/public/uploads';
+        $this->target_page = new Htmldom();
+        $this->site='http://www.artistdirect.com';
     }
     
-    public function store($pars)
-    {        
-        foreach ($pars as $p)
-        {
-            $file=explode("/",$p['photo']);
-            $newfile = $this->upload_dir."/".$file[7];
-            if (!copy($p['photo'], $newfile)) {
-                echo "не удалось скопировать $file[7]...\n";
-            }
-            $p['photo']=$file[7];
-
-            Artist::create($p);
-            exit();
-        }    
-    }
-    
-    public function handle()
+    private function store ($parser_date)
     {
-        //require_once 'simple_html_dom.php';    
+
+        $file=explode("/",$parser_date['photo']);
+        $newfile = $this->upload_dir."/".end($file);
+        if (!copy($parser_date['photo'], $newfile)) {
+            echo "не удалось скопировать". end($file)."...\n";
+        }
+        $parser_date['photo']=end($file);
+        Artist::create($parser_date);
+    }
+    
+    private function parseLinksFromStartPage ()
+    {
+
         $connection = curl_init('http://www.artistdirect.com/music/pop/artists/877');
-        $site='http://www.artistdirect.com';     
+        $pars=array();
+        $info=array();
         //Устанавливаем адрес для подключения, по умолчанию методом GET
         //curl_ - с его помощью подключимся к нужному сайту и спарсим все
         //адреса страниц, информацию с которых нужно сохранить.
         //Затем с помощью curl_multi-* спарсим в режиме многопоточности
         //в массив каждую страницу.
-        //
+        
         curl_setopt($connection, CURLOPT_RETURNTRANSFER,TRUE);
         curl_setopt($connection, CURLOPT_SSL_VERIFYHOST,FALSE);
         curl_setopt($connection, CURLOPT_SSL_VERIFYPEER,FALSE);
-        curl_setopt($connection, CURLOPT_HEADER, TRUE);       
+        curl_setopt($connection, CURLOPT_HEADER, true);       
         curl_setopt($connection, CURLOPT_FOLLOWLOCATION, TRUE);
 
-        //Выполняем запрос
-        //получили код нужной страницы сайта
+        //Выполняем запрос и получаем код нужной страницы сайта в $html
         $html=curl_exec($connection);
-
-        //Завершает сеанс
+        
+        if ($html === FALSE) {
+            echo "cURL Error: " . curl_error($connection);
+        }
+        
+        $info_abt_curl = curl_getinfo($connection);
+        echo 'Took ' . $info_abt_curl['total_time'] . ' seconds for url ' . $info_abt_curl['url'];
+         
+        //Завершаем сеанс
         curl_close($connection);
-
-        //на целевой странице увидели, что ссылки хранятся в абзаце
-        //с class=media
-        $target_page = new Htmldom();
-        $target_page->load($html);
-        $hrefer=$target_page->find('p[class=media]');
-
-        $cmi = curl_multi_init();
-        foreach ($hrefer as $key)
-        {
-            $ci = curl_init($site.$key->first_child ()->href);
-
-            curl_setopt($ci, CURLOPT_URL, $site.$key->first_child ()->href);
-            curl_setopt($ci, CURLOPT_HEADER, false);
-            curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 10);
-            curl_setopt($ci, CURLOPT_TIMEOUT, 10);
-
-            $tasks[$site.$key->first_child ()->href] = $ci;
-            curl_multi_add_handle($cmi, $ci); 
+        $this->target_page->load($html);
+        //на целевой странице увидели, что ссылки хранятся в абзаце с class=media 
+        $hrefer=$this->target_page->find('p[class=media]');
+        return $hrefer;
+    }
+    
+    
+    public function handle()
+    {
+        $hrefer=$this->parseLinksFromStartPage();
+        $mh = curl_multi_init(); //создаем набор дескрипторов cURL
+        foreach ($hrefer as $i=>$v){
+            $link = $this->site.$v->first_child ()->href;
+            $ch[$i] = curl_init($link);
+            curl_setopt($ch[$i], CURLOPT_HEADER, 0);          //Не включать заголовки в ответ
+            curl_setopt($ch[$i], CURLOPT_RETURNTRANSFER, 1);  //Убираем вывод данных в браузер
+            curl_setopt($ch[$i], CURLOPT_CONNECTTIMEOUT, 30); //Таймаут соединения
+            curl_multi_add_handle($mh, $ch[$i]);
         }
+        while (curl_multi_exec($mh, $running) == CURLM_CALL_MULTI_PERFORM); //Запускаем соединения
+        usleep (100000);  //100мс. 
+        $status = curl_multi_exec($mh, $running);
+        //Пока есть незавершенные соединения и нет ошибок мульти-cURL
+        while ($running > 0 && $status == CURLM_OK) {
+            $sel = curl_multi_select($mh, 4); //ждем активность на файловых дескрипторах. Таймаут 4сек
+            usleep (500000);                 //500мс. 
+            //Вдруг cURL хочет быть вызвана немедленно опять..
+            while (($status = curl_multi_exec($mh, $running)) == CURLM_CALL_MULTI_PERFORM);
+            //Если есть завершенные соединения
+            while (($info = curl_multi_info_read($mh))) 
+            {
+                $easyHandle = $info['handle'];    //простой дескриптор cURL
+                $one = curl_getinfo($easyHandle); //получаем инфу по каждому простому дескриптору
+                $httpCode = $one['http_code'];
+                $text = "<br>"."URL: ${one['url']} | HTTP code: $httpCode";
+                flush(); //Сразу выводим инфу в браузер
+                if ($httpCode == 200) {    //если файл/страница успешно получена
+                    $html=null;
+                    $tasks=curl_multi_getcontent($easyHandle);
 
-        $active_flow = null;
-        $pars = array();
+                    $html = new Htmldom();
+                    $html->load($tasks);
 
-        do {
-            //$active_flow - будет меняться
-            $mrc = curl_multi_exec($cmi, $active_flow);
-            curl_multi_select($cmi);        
-        }
-        while ($mrc == CURLM_CALL_MULTI_PERFORM);
+                    $photo=$html->find('div[id=artistPhoto]');
 
-        // выполняем, пока есть активные потоки
-        while ($active_flow && ($mrc == CURLM_OK)) 
-        {
-        // если какой-либо поток готов к действиям
-            if (curl_multi_select($cmi) != -1) {
-                // ждем, пока что-нибудь изменится
-                do 
-                {
-                    $mrc = curl_multi_exec($cmi, $active_flow);
-                    // получаем информацию о потоке
-                    $info = curl_multi_info_read($cmi);
-                    // если поток завершился
-                    if ($info['msg'] == CURLMSG_DONE) {
-                        $ci = $info['handle'];
-                        // ищем урл страницы по дескриптору потока в массиве заданий
-                        $url = array_search($ci, $tasks);
-                        // забираем содержимое
-                        $tasks[$url] = curl_multi_getcontent($ci);
-                        // удаляем поток из мультикурла
-                        curl_multi_remove_handle($cmi, $ci);
-                        // закрываем отдельное соединение (поток)
-                        curl_close($ci);
+                    foreach ($photo as $key){
+                        $title=$key->first_child ()->title;
+                        $src=$key->first_child ()->first_child ()->src;
+                        $pars['name']=$title;
+                        $pars['photo']=$src;   
+                    }
 
-                        $html = new Htmldom();
-                        $html->load($tasks[$url]);
-                        $photo=$html->find('div[id=artistPhoto]');
-
-                        foreach ($photo as $key){
-                            $title=$key->first_child ()->title;
-                            $src=$key->first_child ()->first_child ()->src;
-                            $pars[$title]['name']=$title;
-                            $pars[$title]['photo']=$src;   
+                    $bio=$html->find('div[class=content]');
+                    foreach ($bio as $key)
+                    {
+                        //на странице есть еще один класс "content", избавляемся от него
+                        if ($key->first_child ()->tag==="li"){
+                            continue;
                         } 
+                        $pars['bio']=$key->innertext;
+                    }
 
-                        $bio=$html->find('div[class=content]');
-                        foreach ($bio as $key)
-                        {
-                            //на странице есть еще один класс "content", избавляемся от него
-                            if ($key->first_child ()->tag==="li"){
-                                continue;
-                            } 
-                            $pars[$title]['bio']=$key->innertext;
-                        } 
-                    }   
-                }          
-                while ($mrc == CURLM_CALL_MULTI_PERFORM);
+                    $this->store($pars);
+
+                }elseif($httpCode >= 400){  
+                    $text = "<br>"."URL: ${one['url']} | HTTP code: $httpCode";
+                }
+                curl_multi_remove_handle($mh, $easyHandle);
+                curl_close($easyHandle);
+
             }
-        }
-
-        // закрываем мультикурл
-        curl_multi_close($cmi);
-
-        unset($cmi);
-        unset($mrc);
-        $this->store($pars);
+        } 
     }
 }
+
 
